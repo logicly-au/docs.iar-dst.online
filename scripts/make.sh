907ef8073b7fe266db1f84eb541f3904c2624096
Adam Clarke <adamc@logicly.com.au>
2024-04-15 11:54:40 +1000

    Use vendor sphinx

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,36 +2,7 @@
 
 source version.env
 
-# Exit if anything errors
-set -e
-
-docker pull stratdat/sphinx:production
-
-#docker run \
-#  -v "$(pwd)":/mnt/workdir \
-#  stratdat/sphinx:production make singlehtml
-
-#docker pull stratdat/sphinx-html2pdf:production
-
-#echo "Optimising images"
-#docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir \
-#  --workdir /mnt/workdir/doc/build/singlehtml/_images \
-#  stratdat/sphinx-html2pdf:production \
-#  find . -name *.png -exec pngquant --force --output {} 8 {} \;
-
-#PDF_NAME="${DOC_NAME// /-}-$DOC_VERSION"
-#TIMESTAMP=$(git show -s --format="%ct")
-
-#echo "Building PDF"
-#docker run --rm -e GIT_VERSION -v `pwd`:/mnt/workdir \
-#  stratdat/sphinx-html2pdf:production \
-#  /mnt/workdir/scripts/make-pdf.pl \
-#  --pdf-name "${PDF_NAME}" \
-#  --doc-dir  "/mnt/workdir" \
-#  --timestamp ${TIMESTAMP}
-
-docker run \
- -v "$(pwd)":/mnt/workdir \
- stratdat/sphinx:production make html
-
-#rm -rf build/singlehtml
+docker compose run \
+  --build \
+  --rm sphinx \
+  make html
