fe19d7dab946d4531cb6857f08d1e0064528dd16
Adam Clarke <adamc@logicly.com.au>
2024-04-15 13:33:24 +1000

    Move RTD content to doc dir

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-source version.env
+# Exit if anything errors
+set -euo pipefail
 
+pushd .
+cd doc
+
+source version.env
+GIT_VERSION=$(git describe --tags --always)
+
+echo "Building HTML"
 docker compose run \
   --build \
+  -e GIT_VERSION \
   --rm sphinx \
   make html
+
+popd
