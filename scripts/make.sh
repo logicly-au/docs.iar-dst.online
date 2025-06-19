dc9794998e02238eae8293398ecbd5b1e249615b
Adam Clarke <adamc@logicly.com.au>
2025-06-19 11:30:28 +1000

    More modern structure

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,19 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Exit if anything errors
 set -euo pipefail
 
+export INDEX="file:///$(pwd)/docs/build/html/index.html"
+
 pushd .
 cd docs
 
-source version.env
+# shellcheck disable=SC2034
 GIT_VERSION=$(git describe --tags --always)
 
-echo "Building HTML"
-docker compose run \
-  --build \
-  -e GIT_VERSION \
-  --rm sphinx \
-  make html
+echo "Building HTML document ... "
+
+docker compose --progress=quiet run --build --rm sphinx make html
+
+echo "Done: Building HTML document - ${INDEX}"
+
+open "${INDEX}"
 
 popd
+
+echo "Done"
+
